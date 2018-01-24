075c7c6ca14b0d9aec6f5c4466c139fdc33be9af
Abhishek Srivastava <abhsrivastava@researchnow.com>
2018-01-24 10:49:20 -0600

    models don't import circe generic auto

--- src/main/scala/MyModels.scala
+++ src/main/scala/MyModels.scala
@@ -1,7 +1,6 @@
 package com.abhi.models
 
 import io.circe.generic.JsonCodec
-import io.circe._, io.circe.syntax._, io.circe.parser._, io.circe.generic.auto._
 
 @JsonCodec trait Foo
 final case class Bar(name: String) extends Foo
